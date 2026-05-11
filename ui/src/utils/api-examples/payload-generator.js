e20355a7b98a2b92f64009f2b55e37bafb91a5d2
Bastien Orivel <borivel@mozilla.com>
2026-05-11 14:20:01 +0200

    noUnusedVariables

--- ui/src/utils/api-examples/payload-generator.js
+++ ui/src/utils/api-examples/payload-generator.js
@@ -671,7 +671,7 @@ export default function generatePayloadExample(schema, allSchemas = []) {
       schema.$id,
       new Set()
     );
-  } catch (error) {
+  } catch (_error) {
     // Error generating payload example
     return null;
   }
